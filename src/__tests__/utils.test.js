91ff79688df37ee2d76c9284289e4de7ac6c224d
Edwin kipyego <koriredwin95@gmail.com>
2025-04-20 12:22:42 +0300

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
-// Your tests here
+
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  it('returns true for a simple palindrome', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  it('returns false for a non-palindrome', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  it('is case insensitive', () => {
+    expect(isPalindrome('Mom')).toBe(true);
+  });
+
+  it('returns false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  it('throws an error for words with non-alphabetic characters', () => {
+    expect(() => isPalindrome('mom!')).toThrow('Input must contain only alphabetic characters.');
+  });
+
+  it('throws an error for non-string input', () => {
+    expect(() => isPalindrome(123)).toThrow('Input must be a string.');
+  });
+});
